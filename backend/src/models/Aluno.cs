92f85980919086e8ad378e69fd4137d283c27abf
eduardotimm <etimmbuonacura@gmail.com>
2025-09-19 17:08:27 -0300

    Refactor Aluno and GrupoDeEstudo models to ensure proper data annotations and validation rules

--- backend/src/models/Aluno.cs
+++ backend/src/models/Aluno.cs
@@ -2,11 +2,28 @@ namespace ProjetoConexxa.Models
 {
     public class Aluno
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
         public string NomeCompleto { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
         public string EmailInstitucional { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Curso { get; set; }
+
+        [Required]
+        [Range(1, 10)]
         public int Periodo { get; set; }
+
+        [Required]
+        [MinLength(6)]
         public string Senha { get; set; }
     }
 }
--- backend/src/models/GrupoDeEstudo.cs
+++ backend/src/models/GrupoDeEstudo.cs
@@ -4,12 +4,26 @@ namespace ProjetoConexxa.Models
 {
     public class GrupoDeEstudo
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Curso { get; set; }
-        public string Periodo { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int Periodo { get; set; }
+
         public List<Aluno> Membros { get; set; }
+
         public string FotoPerfilUrl { get; set; }
+
+
         public string Descricao { get; set; }
 
         public GrupoDeEstudo()
